06e2afe53547f9b2e1fd52c461dfbe450edf4454
Akbar Septriyan <Akbar Septriyan>
2016-01-08 21:34:16 +0700

    revisi

--- login.php
+++ login.php
@@ -1,36 +1,7 @@
-<?php session_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <title>The Power Of Bootstrap</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type = "text/css" href="bootstrap/css/bootstrap.min.css">
-  <link rel="stylesheet" type = "text/css" href="css/csssignin.css">
-</head>
-
-  <?php
- 
-    if(isset($_SESSION["userlogin"])){
-        header("Location: home.php");
-      }
-      $errormsg = "";    
-      if(isset($_POST['username'])) {  
-      if (cekuser($_POST['username'])) {
-        if(login($_POST['username'], $_POST['password'])) {
-          echo "Berhasil";
-          $_SESSION["userlogin"] = $_POST["username"];
-          header("Location: home.php");
-          }
-          else {
-          $errormsg = "password tidak valid";
-          }
-        }
-        else {
-          $errormsg = "username tidak valid";
-        }
-      }   
-      function connectDB() {
+<?php 
+   session_start();
+   
+   function connectDB() {
         $servername = "localhost";
         $username = "himassui_akbar";
         $password = "asepwhite";
@@ -40,11 +11,11 @@
             die("Connection failed: " . mysqli_connect_error());
         }
         return $conn;
-      }
-
-      function login($username, $password){
+   }
+   
+   function login($username, $password){
           $conn = connectDB();
-          $sql = "SELECT Username, Password FROM peserta WHERE Username='$username' AND Password='$password'";
+          $sql = "SELECT username, password FROM peserta WHERE username='$username' AND password='$password'";
           $result = mysqli_query($conn, $sql);
           if(mysqli_num_rows($result) > 0) {
              mysqli_close($conn);
@@ -55,7 +26,7 @@
 
       function cekuser($username){
           $conn = connectDB();
-          $sql = "SELECT username FROM users WHERE username='$username'";
+          $sql = "SELECT username FROM peserta WHERE username='$username'";
           $result = mysqli_query($conn, $sql);
           if(mysqli_num_rows($result) > 0) {
              mysqli_close($conn);
@@ -63,9 +34,35 @@
           }
           return false;
       }
-
-  ?>
-
+      
+      if(isset($_SESSION["userlogin"])){
+       header('Location: http://www.himassui.com/UIGTB2016/index.html');
+      }
+      $errormsg = "";    
+      if(isset($_POST['username'])) {  
+      if (cekuser($_POST['username'])) {
+        if(login($_POST['username'], $_POST['password'])) {
+          $_SESSION["userlogin"] = $_POST["username"];
+          header('Location: http://www.himassui.com/');
+          }
+          else {
+          $errormsg = "password tidak valid";
+          }
+        }
+        else {
+          $errormsg = "username tidak valid";
+        }
+      }   
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>The Power Of Bootstrap</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type = "text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type = "text/css" href="css/csssignin.css">
+</head>
 <body> 
   <div class = "container">
     <form class="form-signin" action = "" method = "POST">
@@ -75,7 +72,7 @@
           <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button><br>
           <a href = "registration.php"><button class="btn btn-lg btn-primary btn-block" type="button">Register</button></a>
     </form>
-     <span><?php echo $errormsg; ?> </span><br>
+    
   </div>
 </body>
 <script src = "script/jquery-2.1.4.min.js"> </script>
