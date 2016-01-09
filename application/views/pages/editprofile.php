09bba1f3264a9459afcce2b2d513c8f4f6225ca1
Akbar Septriyan <Akbar Septriyan>
2016-01-09 23:49:49 +0700

    dimanamana

---
+++ application/views/pages/editprofile.php
@@ -0,0 +1,106 @@
+<?php
+    
+    session_start();
+      
+      $errormsg = "";
+      $newnama = "";
+      $newasal = "";
+      $newjurusan = "";
+      $newemail = "";
+      $newhp = "";
+      $flag = false;
+      $username = $_SESSION["userlogin"];
+
+      getattribute($username);
+
+      if(isset($_POST['nama'])) {  
+        $newnama = $_POST['nama'];
+        $flag = true;
+      }
+
+      if(isset($_POST['asal'])) {  
+        $newnama = $_POST['asal'];
+        $flag = true;
+      }
+
+      if(isset($_POST['jurusan'])) {  
+        $newjurusan = $_POST['jurusan'];
+        $flag = true;
+      }
+
+      if(isset($_POST['email'])) {  
+        $newemail = $_POST['email'];
+        $flag = true;
+      }
+
+      if(isset($_POST['hp'])) {  
+        $newhp = $_POST['hp'];
+        $flag = true;
+      }
+
+      if ($flag) {
+        echo "yeey";
+        $conn = connectDB();
+        echo $newnama;
+        echo $newasal;
+        echo $newjurusan;
+        echo $newemail;
+        echo $newhp;
+        $sql = "UPDATE peserta SET asal = 'bingung' WHERE username = '$username'";
+        mysqli_query($conn, $sql);
+      }   
+     function connectDB() {
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "himassui_to2016";
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+        return $conn;
+     }
+
+      function getattribute($username){
+          $conn = connectDB();
+          $sql = "SELECT * FROM peserta WHERE username='$username'";
+          $result = mysqli_query($conn, $sql);
+          while ($row = mysqli_fetch_assoc($result)) {
+            echo "loli";
+          $newnama = $row['nama'];
+          $newasal = $row['asal'];
+          $newjurusan = $row['jurusan'];
+          $newemail = $row['email'];
+          $newhp = $row['nama'];
+          }
+      }
+
+  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>The Power Of Bootstrap</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type = "text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type = "text/css" href="css/csssignin.css">
+</head>
+<body> 
+  <div class = "container">
+    <form class="form-signin" action = "" method = "POST">
+          <h2 class="form-signin-heading">Edit Profile</h2>
+          <input name="nama" class="form-control" placeholder="Nama">          
+          <input name="asal" class="form-control" placeholder="Asal Sekolah">
+          <input name="jurusan" class="form-control" placeholder="Jurusan">
+          <input type = "email" name="email" class="form-control" placeholder="Email">
+          <input name="hp" class="form-control" placeholder="Nomor HP">
+          <button class="btn btn-lg btn-primary btn-block" type="submit">Edit</button>
+    </form>
+     <span><?php echo $errormsg; ?> </span><br>
+  </div>
+</body>
+<script src = "script/jquery-2.1.4.min.js"> </script>
+<script src = "bootstrap/js/bootstrap.min.js"> </script>
+<script src = "script/jsminimum.js"> </script>
+</html>
--- application/views/pages/registration.php
+++ application/views/pages/registration.php
@@ -6,24 +6,27 @@
       $errormsg = "";    
       if(isset($_POST['username'])) {  
         if (cekuser($_POST['username'])) {
-            $errormsg = "username telah digunakan";
+            $errormsg = "Username telah digunakan";
         }
         else if (!preg_match("/^[a-z]+$/", $_POST["username"])) {
-            $errormsg = "username harus huruf kecil";
+            $errormsg = "Username harus huruf kecil";
         }
         else if (!((preg_match("/^[a-z]+[0-9]+$/", $_POST["password"])) || (preg_match("/^[0-9]+[a-z]+$/", $_POST["password"]))) ){
-            $errormsg = "password harus merupakan kombinasi huruf kecil dan angka";
+            $errormsg = "Password harus merupakan kombinasi huruf kecil dan angka";
+        }
+        else if (!preg_match("/^[0-9]+$/", $_POST["hp"])) {
+            $errormsg = "Nomor hp harus merupakan angka!";
         }
         else {
-          insertuser($_POST['username'], $_POST['password']);
+          insertuser($_POST['username'], $_POST['password'], $_POST['nama'], $_POST['asal'], $_POST['email'], $_POST['hp'], $_POST['jurusan']);
           session_start();
           $_SESSION['userlogin'] = $_POST['username'];
            header("Location: home");
         }
       }
-      function insertuser($username, $password) {
+      function insertuser($username, $password, $nama, $asal, $email, $hp, $jurusan) {
         $conn = connectDB();
-        $sql = "INSERT into peserta (username, password) VALUES('$username','$password')";
+        $sql = "INSERT into peserta (username, password, nama, asal, email, hp, jurusan) VALUES('$username','$password', '$nama', '$asal', '$email', '$hp', '$jurusan')";
         mysqli_query($conn, $sql);
       }   
      function connectDB() {
@@ -68,7 +71,7 @@
           <input type="password" name="password" class="form-control" placeholder="Password" required>
           <input name="nama" class="form-control" placeholder="Nama" required>          
           <input name="asal" class="form-control" placeholder="Asal Sekolah" required>
-          <input name="username" class="form-control" placeholder="Jurusan" required>
+          <input name="jurusan" class="form-control" placeholder="Jurusan" required>
           <input type = "email" name="email" class="form-control" placeholder="Email" required>
           <input name="hp" class="form-control" placeholder="Nomor HP" required>
           <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
