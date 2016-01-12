adc75319645fde560653d5eb971fd29acd99799d
grgp <g.a.pitoy@gmail.com>
2016-01-12 22:17:25 +0700

    unsuccesfully tried to merge bootstrap theme

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,34 +1,57 @@
-<body>
 
 	<div class="container" id="main">
 
-    <div class="jumbotron">
-      <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-      <p><a class="btn btn-lg btn-success" href="#" role="button">Sign up today</a></p>
-    </div>
+    <header>
+        <div class="container">
+            <div class="intro-text">
+                <div class="intro-lead-in">Welcome To Our Studio!</div>
+                <div class="intro-heading">It's Nice To Meet You</div>
+                <a href="#services" class="page-scroll btn btn-xl">Tell Me More</a>
+            </div>
+        </div>
+    </header>
 
-    <div class="row marketing">
-      <div class="col-lg-6">
-        <h4>Subheading</h4>
-        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-        <h4>Subheading</h4>
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-        <h4>Subheading</h4>
-        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+    <section id="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 text-center">
+            <h2 class="section-heading">Timeline</h2>
+            <hr class="primary">
+          </div>
+        </div>
       </div>
-
-      <div class="col-lg-6">
-        <h4>Subheading</h4>
-        <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-        <h4>Subheading</h4>
-        <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-        <h4>Subheading</h4>
-        <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-      </div>
-    </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-diamond wow bounceIn text-primary"></i>
+              <h3>Sturdy Templates</h3>
+              <p class="text-muted">Our templates are updated regularly so they don't break.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s"></i>
+              <h3>Ready to Ship</h3>
+              <p class="text-muted">You can use this theme as is, or you can make changes!</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+              <h3>Up to Date</h3>
+              <p class="text-muted">We update dependencies to keep things fresh.</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 text-center">
+            <div class="service-box">
+              <i class="fa fa-4x fa-heart wow bounceIn text-primary" data-wow-delay=".3s"></i>
+              <h3>Made with Love</h3>
+              <p class="text-muted">You have to make your websites with love these days!</p>
+            </div>
+          </div>
+        </div>
+        </div>
+    </section>
 
   </div> <!-- /container -->
--- application/views/pages/kartu.php
+++ application/views/pages/kartu.php
@@ -1,45 +1,25 @@
 	<body>
-<<<<<<< HEAD
-		<center>
-			<title>#TestHimas</title>
-			<br><br>
-			<form action="imggen" method="post" enctype="multipart/form-data">
-				<br>
-				Nama Lengkap:
-				<br>
-				<input name="name" placeholder="e.g. Akbar Septriyan" required type="text">
-				<br>
-				Nomor Ujian:
-				<br>
-				<input name="id_num" placeholder="e.g. 1405459223" required type="text">
-				<br>
-				Asal Sekolah:
-				<br>
-				<input name="school" placeholder="e.g. SMAN 1 Jakarta" required type="text">
-				<br>
-				<input type="file" name="selectedFile">
-                <br>
-		    	<p>(photo must be in JPEG or PNG format and under 5 MB)</p>
-                <br>
-				<br><br>
-				<input class="btn btn-default" name="submit" type="submit" value="Submit">
-			</form>
-		<div id="footer">
-		  <i>PHP by <a href="http://www.awasisto.com">Andika Wasisto</a><br>HTML & CSS by <a href="http://www.veltastolkien.com">Velta Azizah</a><br><br>© Orion 2014</i>
-=======
+
 		<div class="container">
 			<div class="panel panel-default logpanel center">
 	  		<div class="panel-body">
 				
 					<form class="form-signin" action="imggen" method="post" enctype="multipart/form-data">
 						<h2 class="form-signin-heading">Masukkan keterangan Anda</h2>
-						<input name="name" placeholder="e.g. Akbar Septriyan" required type="text" class="form-control">
-						<input name="id_num" placeholder="e.g. 1405459223" required type="text" class="form-control">
-						<input name="school" placeholder="e.g. SMAN 1 Jakarta" required type="text" class="form-control">
+						<label>Nama</label>
+						<input name="name" placeholder="cth. Akbar Septriyan" required type="text" class="form-control">
+						<label>No. Ujian</label>
+						<input name="id_num" placeholder="cth. 1405459223" required type="text" class="form-control">
+						<label>Asal Sekolah</label>
+						<input name="school" placeholder="cth. SMAN 1 Jakarta" required type="text" class="form-control">
+						<label>Gambar Pas Foto</label>
+						<input type="file" name="selectedFile">
+            <br>
+		    		<p>(photo must be in JPEG or PNG format and under 5 MB)</p>
+            <br>
 						<input class="btn btn-lg btn-primary btn-block" name="submit" type="submit" value="Submit">
-					</form>
 
+					</form>
+				</div>
 			</div>
->>>>>>> 58ee73cd9b299a75b401b346a66ecacbb05ac8de
 		</div>
-	</div>
