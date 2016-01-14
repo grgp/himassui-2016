007a2ffbd724bbfdf6d9f3fe5c072bad20992841
grgp <g.a.pitoy@gmail.com>
2016-01-14 20:32:37 +0700

    footernya dipercantik, semoga di chrome tidak kacau

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,18 +1,22 @@
 	<aside class="bg-dark">
     <div class="container text-center" id="bawah">
-      <div class="col-lg-8 col-lg-offset-2 text-center">
-      <h2 class="section-heading">Sponsored By</h2>
-      <hr class="primary">
-      <img src="assets/img/bi.png" class="img-responsive" alt="" width = "500px">
-      <img src="assets/img/sumsel.png" class="img-responsive" alt="" width = "500px">
-      <h2 class="section-heading">Media Partners</h2>
-      <hr class="primary">
-      <span>
-      <img src="assets/img/rri.png" class="img-responsive" alt="" width = "100px">
-      <img src="assets/img/bt.jpeg" class="img-responsive" alt="" width = "100px">
-      <img src="assets/img/jess.png" class="img-responsive" alt="" width = "100px">
-      <img src="assets/img/sono.png" class="img-responsive" alt="" width = "100px">
-      </span>
+      <div class="col-lg-10 col-lg-offset-1 text-center">
+        <div class="sponsor col-lg-6">
+          <h3 class="section-heading">Sponsored By</h3>
+          <hr class="primary">
+          <img src="assets/img/bi.png" class="img-responsive" alt="" width = "500px">
+          <img src="assets/img/sumsel.png" class="img-responsive" alt="" width = "500px">
+        </div>
+        <div class="medpar col-lg-6">
+          <h3 class="section-heading">Media Partners</h3>
+          <hr class="primary">
+          <span>
+          <img src="assets/img/rri.png" class="img-responsive" alt="" width = "100px">
+          <img src="assets/img/bt.jpeg" class="img-responsive" alt="" width = "100px">
+          <img src="assets/img/jess.png" class="img-responsive" alt="" width = "100px">
+          <img src="assets/img/sono.png" class="img-responsive" alt="" width = "100px">
+          </span>
+        </div>
       </div>
     </div>
   </aside>
